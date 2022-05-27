e439fc1d3eb312b32ab01a74197196b6f54ef81c
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-27 18:12:59 +0300

    it will the gallery view

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,9 @@
-from turtle import title
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
     title='my gallery'
-    return render(request,'main/index.html',locals)
+    return render(request,'gallery_templates/index.html',locals())
+
+def gallery(request):
+    return render(request,'gallery_templates/gallery.html',locals())
