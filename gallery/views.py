dd9dfa8e0d85f97d6d84728422fc38d0b4bc5c8b
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-27 14:56:04 +0300

    it will create the indexview

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,7 @@
+from turtle import title
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    title='my gallery'
+    return render(request,'main/index.html',locals)
