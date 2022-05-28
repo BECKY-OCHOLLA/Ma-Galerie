27bc5a4ed94b3f0c145228c281a386657572dba0
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-28 23:04:54 +0300

    it willadd the image model

--- gallery/models.py
+++ gallery/models.py
@@ -4,10 +4,24 @@ from django.db import models
 class location(models.Model):
     name=models.CharField(max_length=50)
 
-    @classmethod
-    def get_locations(cls):
-      locations = Location.objects.all()
-      return locations
+    
 
     def __str__(self):
       return self.name
+
+class category(models.Model):
+    name=models.CharField(max_length=50)
+
+
+
+    def __str__(self):
+      return self.name
+
+class Image(models.Model):
+    name=models.CharField(max_length=50)
+    image=models.ImageField(upload_to='Images/',null=True ,blank=True)
+    description=models.TextField()
+    category=models.ForeignKey(category,on_delete=models.CASCADE,)
+    location=models.ForeignKey(location,on_delete=models.CASCADE,)
+    author=models.CharField(max_length=40, default='admin')
+    date=models.DateTimeField(auto_now_add=True)
