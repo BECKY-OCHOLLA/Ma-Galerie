fb0d0beb7f853426734cd08aa5e3662a708f1538
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-29 01:00:35 +0300

    it willregister admin site

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import location,category,image
+
+
 
 # Register your models here.
+admin.site.register(image)
+admin.site.register(location)
+admin.site.register(category)
