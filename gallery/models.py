8703f20748686ed0eb7f8cea419a9fc1406d6069
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-28 14:46:11 +0300

    it will add class location

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class location(models.model):
+class location(models.Model):
     name=models.CharField(max_length=50)
+
+    @classmethod
+    def get_locations(cls):
+      locations = Location.objects.all()
+      return locations
+
+    def __str__(self):
+      return self.name
--- main/settings.py
+++ main/settings.py
@@ -84,9 +84,6 @@ DATABASES = {
     'PASSWORD':'access',
     }
 }
-
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
