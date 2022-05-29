620c776221ea80546645aaba0c822f73e31c50fd
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-29 12:10:29 +0300

    it will update index.html

--- gallery/models.py
+++ gallery/models.py
@@ -9,6 +9,16 @@ class location(models.Model):
     def __str__(self):
       return self.name
 
+    @classmethod
+    def update_location(cls, id, value):
+      cls.objects.filter(id=id).update(image=value)
+
+    def save_location(self):
+      self.save()
+
+    def delete_location(self):
+      self.delete()  
+
 class category(models.Model):
     name=models.CharField(max_length=50)
 
@@ -16,6 +26,17 @@ class category(models.Model):
 
     def __str__(self):
       return self.name
+
+
+    @classmethod
+    def update_category(cls, id, value):
+      cls.objects.filter(id=id).update(image=value)
+
+    def save_category(self):
+       self.save()
+
+    def delete_category(self):
+      self.delete()
 
 class image(models.Model):
     name=models.CharField(max_length=50)
